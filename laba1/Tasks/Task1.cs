0fb37697382a45995ae38e9b51d9c70087656bf0
Kyryl <pozniak17kirill@gmail.com>
2026-02-26 17:40:30 +0200

    update2

--- laba1/Tasks/Task1.cs
+++ laba1/Tasks/Task1.cs
@@ -12,7 +12,7 @@ namespace laba1.Tasks
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
 
-            List<double> I = new List<double> { 0.5, 1.0, 1.5, 2.0, 2.5 };
+            List<double> I = new List<double> { 12, 1.0, 1.5, 2.0, 2.5 };
             List<double> U = new List<double> { 1.1, 2.1, 3.2, 4.1, 5.2 };
 
             if (I.Count != U.Count)
@@ -24,6 +24,8 @@ namespace laba1.Tasks
             // Метод найменших квадратів
             double sumIU = I.Zip(U, (i, u) => i * u).Sum();
             double sumII = I.Select(i => i * i).Sum();
+
+            
 
             double R = sumIU / sumII;
 
